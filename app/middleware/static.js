2ee40fdce280fc8e00875fc8572f8c668c6bd0d1
AutoSponge <pgrenier@gmail.com>
2014-07-24 14:58:48 -0400

    added default /public static folder and middleware

---
+++ app/middleware/static.js
@@ -0,0 +1,2 @@
+var express = require( 'express' );
+module.exports = express.static( __dirname + '/../public' );
--- app/routers/default/routes/get.js
+++ app/routers/default/routes/get.js
@@ -1,6 +1,6 @@
 'use strict';
 module.exports = function ( router ) {
-    router.get( '/', function ( req, res/*, next*/ ) {
-        res.send( '/ GET successful...' );
+    router.get( '/get', function ( req, res/*, next*/ ) {
+        res.send( '/GET successful...' );
     } );
 };
